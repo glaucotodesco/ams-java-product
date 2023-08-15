afeb5ab1de6c32cd4200158edd44dba954d4c0d9
glauco.todesco <glauco.todesco@gmail.com>
2023-08-15 16:47:07 -0300

    aula-15-08

---
+++ src/main/java/com/example/productbackend/resources/ProductContoller.java
@@ -0,0 +1,41 @@
+package com.example.productbackend.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.productbackend.entities.Product;
+import com.example.productbackend.services.ProductService;
+
+@RestController
+@RequestMapping("products")
+public class ProductContoller {
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getProducts(){
+        var products = this.service.getProducts();
+        return ResponseEntity.ok(products);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable long id){
+        var product = this.service.getProduct(id);
+        return ResponseEntity.ok(product);
+    }
+
+
+}
+
+
+
+
+
+
---
+++ src/main/java/com/example/productbackend/services/ProductService.java
@@ -0,0 +1,35 @@
+package com.example.productbackend.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.productbackend.entities.Product;
+import com.example.productbackend.repositories.ProductRepository;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class ProductService {
+    
+    @Autowired
+    private ProductRepository repository;
+
+    public List<Product> getProducts(){
+        return this.repository.findAll();
+    }
+
+    public Product getProduct(long id){
+        return this.repository
+                   .findById(id)
+                   .orElseThrow(
+                     () -> new EntityNotFoundException("Product not found")
+                   );
+    }
+
+
+}
+
+
+
