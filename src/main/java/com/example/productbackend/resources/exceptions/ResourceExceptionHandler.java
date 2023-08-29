1944abc9658ef8b99114e0b16fd66a8cf26ec885
glauco.todesco <glauco.todesco@gmail.com>
2023-08-29 15:03:38 -0300

    Validations

--- src/main/java/com/example/productbackend/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/example/productbackend/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -14,7 +15,7 @@ import jakarta.servlet.http.HttpServletRequest;
 public class ResourceExceptionHandler {
     
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<StandardError>entityNotFoundEXception(
+    public ResponseEntity<StandardError>entityNotFoundException(
                                      EntityNotFoundException e,
                                      HttpServletRequest request)
     {
@@ -27,6 +28,27 @@ public class ResourceExceptionHandler {
         error.setMessage(e.getMessage());
         error.setPath(request.getRequestURI());
         
+        return ResponseEntity.status(status).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationErrors> validationErrorsException(
+                                     MethodArgumentNotValidException e,
+                                     HttpServletRequest request)
+    {
+        ValidationErrors error = new ValidationErrors();
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+
+        error.setTimeStamp(Instant.now());
+        error.setStatus(status.value());
+        error.setError("Validation Errors");
+        error.setMessage(e.getMessage());
+        error.setPath(request.getRequestURI());
+
+        e.getBindingResult()
+         .getFieldErrors()
+         .forEach( err -> error.addError(err.getDefaultMessage()));
+
         return ResponseEntity.status(status).body(error);
     }
 }
